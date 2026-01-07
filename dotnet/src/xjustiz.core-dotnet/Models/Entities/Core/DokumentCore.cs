02bc75dba34b9ff15c53717b973b1454f963761a
Samuel Stauß <samuel.stauss@justin-legal.com>
2026-01-07 09:48:18 +0100

    fix build errors

--- dotnet/src/xjustiz.core-dotnet/Models/Entities/Core/DokumentCore.cs
+++ dotnet/src/xjustiz.core-dotnet/Models/Entities/Core/DokumentCore.cs
@@ -1,7 +1,7 @@
 namespace xjustiz.core_dotnet.Models.Entities.Core;
 
 using System.Xml.Serialization;
-using xjustiz.core_dotnet.Models.Codes;
+using xjustiz.core_dotnet.Models.Helpers;
 using xjustiz.core_dotnet.Util.Versioning;
 
 /// <summary>
